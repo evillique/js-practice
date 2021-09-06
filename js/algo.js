51e19c7e2e12543eeec167a96926bb43c8708b4b
Nikolay Degterinsky <evillique@gmail.com>
2021-09-06 12:46:56 +0300

    New version

--- js/algo.js
+++ js/algo.js
@@ -1,6 +1,9 @@
+const ms_between_layers = 800;
+
 let cur_ones = 0;
-const max_precision = 10;
-const ms_between_layers = 1200;
+let cur_line = 0;
+let max_lines = 0;
+let max_precision = 10;
 
 //// Plotter options
 let options = {
@@ -14,6 +17,15 @@ let options = {
 
 let sun = new Plotter("plot", options);
 const button = document.querySelector('button');
+const slider = document.getElementById('precision');
+
+slider.addEventListener("change", changeParameters);
+
+function changeParameters(event) {
+    let n = parseInt(event.target.value);
+    max_precision = Math.min(n, 10);
+    max_lines = Math.max(n - 10, 0)
+}
 
 //// List of negative powers of 3
 let three_pow = [];
@@ -24,7 +36,7 @@ for (let i = 1; i <= max_precision; ++i) {
 //// Main function that evaluates x and draws points
 function recursion(ones, k, x) {
     if (k === max_precision) {
-        sun.addPoint(x, y, {size: 1.5});
+        sun.addPoint(x, y, {size: 1.1});
     } else if (k + ones === max_precision) {
         recursion(ones - 1, k + 1, x + three_pow[k])
     } else {
@@ -42,14 +54,28 @@ function print() {
     let startTime = performance.now();
 
     // Evaluate y for current layer
-    y = 1.0/(cur_ones + 1);
-    recursion(cur_ones, 0, 0);
+    y = 1.0 / (cur_ones + cur_line + 1);
+
+    if (cur_ones === 5 && cur_line < max_lines) {
+        sun.addFunc(function () {
+            return y;
+        }, {color: 6,
+            strokeWidth: 2.3,
+            left: 0,
+            right: 1,
+            top: 1.03,
+            bottom: 0});
+        ++cur_line;
+    } else {
+        recursion(cur_ones, 0, 0);
+        ++cur_ones;
+    }
 
     let endTime = performance.now();
     let timeDiff = endTime - startTime;
     console.log(timeDiff + " ms");
 
-    if (++cur_ones > max_precision) {
+    if (cur_ones + cur_line > max_precision + max_lines) {
         return;
     }
 
@@ -60,6 +86,7 @@ function print() {
 
 function start() {
     button.disabled = true;
+    slider.disabled = true;
 
     setTimeout(function () {
         print();
